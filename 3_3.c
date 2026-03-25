c0d64c46eed2468eba8160962182a6c906b65542
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-03-25 14:55:29 +0800

    Add basic float variable example in 3_3.c

---
+++ 3_3.c
@@ -0,0 +1,11 @@
+/*3_3.變數與指定運算子-指定浮點數(20.0)*/
+#include <stdio.h>
+#include<math.h>
+
+int main() {
+    float num1=10.100001;
+    float num2=5.200002;
+    printf("%f\n%f",num2,num1);
+
+   return 0;
+}
