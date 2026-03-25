0f1df6be94a40058eae9779a402f9b8512824b2e
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-03-25 15:15:34 +0800

    Create 3_4.c

---
+++ 3_4.c
@@ -0,0 +1,12 @@
+/*3_4.變數與指定運算子-整數與浮點數(20.0)*/
+#include <stdio.h>
+#include<math.h>
+int main() {
+    int height=170;
+    float weight=55.222;
+    
+    printf("My height is %d cm.\n",height);
+    printf("My weight is %.3f kg.",weight);
+
+   return 0;
+}
