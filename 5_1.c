8c43204a930838178f66197f1239f77c86104a03
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-03-25 22:46:30 +0800

    Implement basic arithmetic operations in 5_1.c

---
+++ 5_1.c
@@ -0,0 +1,12 @@
+/*5_1.算術運算子-四則運算(20.0)*/
+#include <stdio.h>
+int main() {
+    int num1,num2;
+    scanf("%d%d",&num1,&num2);
+    printf("%d+%d=%d\n",num1,num2,num1+num2);
+    printf("%d-%d=%d\n",num1,num2,num1-num2);
+    printf("%d*%d=%d\n",num1,num2,num1*num2);
+    printf("%d/%d=%d\n",num1,num2,num1/num2);
+
+   return 0;
+}
