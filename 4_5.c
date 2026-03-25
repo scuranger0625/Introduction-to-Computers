2ee82b4bee1000312307f9959f0cd6a5b048aac7
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-03-25 22:41:34 +0800

    Add trapezoid input function in 4_5.c

---
+++ 4_5.c
@@ -0,0 +1,9 @@
+/*4_5.輸入函式-梯形上下底與高(20.0)*/
+#include <stdio.h>
+int main() {
+    int upper,lower,high;
+    scanf("%d%d%d",&upper,&lower,&high);
+    printf("%d %d %d",upper,lower,high);
+
+   return 0;
+}
