eeee0936af4568e760abeabb4a906260385cc126
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-03-26 02:01:18 +0800

    Add basic integer addition program in C

---
+++ 7_7.c
@@ -0,0 +1,11 @@
+/*7_7.文字型態與輸出(for Python)-分割字串轉整數(5.0)*/
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    
+    c=a+b;
+    printf("%d+%d=%d",a,b,c);
+
+   return 0;
+}
