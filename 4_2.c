a192b604181c16ac53017542ba7b5780b626d342
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-03-25 16:53:53 +0800

    Add program to input and print three integers

---
+++ 4_2.c
@@ -0,0 +1,11 @@
+/*4_2.輸入函式-3個整數(20.0)*/
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    printf("%d %d %d",c,b,a);
+
+   return 0;
+}
