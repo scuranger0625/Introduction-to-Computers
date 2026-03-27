3dbc2196a5c9692c44310d49029e90959a508ab7
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-03-27 23:19:24 +0800

    Create 9_8.c

---
+++ 9_8.c
@@ -0,0 +1,12 @@
+/*9_8.循序結構隨堂測驗-南極企鵝大遷徙(10.0)*/
+
+// 關冷氣救企鵝? 他吹幾萬年了 該換我了吧
+#include <stdio.h>
+int main() {
+    double a,b;
+    // double 要用 %lf
+    scanf("%lf%lf",&a,&b);
+    printf("%.2f",a+b);
+    
+   return 0;
+}
