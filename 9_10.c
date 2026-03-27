7c3959650ed02a0f6f8cf2614e6950d7ada76479
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-03-27 23:53:12 +0800

    Update 9_10.c

--- 9_10.c
+++ 9_10.c
@@ -6,7 +6,6 @@ int main() {
     double r;       // 注意事項：一定要先宣告r的變數 變數volume才能用r
     double volume; // 注意事項：參數和傳回值皆為 double 類型
 
-    
     scanf("%lf",&r);
     volume=(4.0/3.0)*(3.14)*(pow(r,3.0)); // 4/3 要寫成 4.0/3.0（避免整數除法）
     printf("%.2f",volume);
