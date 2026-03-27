6d77af8aff2d188c88a4b73fbd99cd42c0fa4453
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-03-27 22:49:35 +0800

    Create 9_3.c

---
+++ 9_3.c
@@ -0,0 +1,11 @@
+/*9_3.循序結構隨堂測驗-03:計算(a+b)/c的值(10.0)*/
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    
+    int result=(a+b)/c;
+    printf("%d",result);
+    
+   return 0;
+}
