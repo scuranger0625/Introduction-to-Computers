367558e812ba69b885e118a9ee471c50c1371fff
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-03-27 22:39:16 +0800

    Create 9_2.c

---
+++ 9_2.c
@@ -0,0 +1,10 @@
+/*9_2.循序結構隨堂測驗-02:計算(a+b)*c的值(10.0)*/
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    
+    int result=(a+b)*c;
+    printf("%d",result);
+   return 0;
+}
