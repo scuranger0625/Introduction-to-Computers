c3876bfa132a5682a9c783c07cf46d00dd0ac6b8
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-03-27 21:50:21 +0800

    Create 8_8.c

---
+++ 8_8.c
@@ -0,0 +1,13 @@
+/*8_8.位元運算子-左移右移(10.0)*/
+#include <stdio.h>
+int main() {
+    int m;
+    scanf("%d",&m);
+    
+    printf("%d<<2=%d\n",m,m<<2);
+    printf("%d>>2=%d\n",m,m>>2);
+    printf("%d<<4=%d\n",m,m<<4);
+    printf("%d>>4=%d\n",m,m>>4);
+
+   return 0;
+}
