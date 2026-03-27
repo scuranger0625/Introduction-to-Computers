1fc01f00a6c80ca7a0f3a4b9caad8e7a97aaef6c
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-03-27 21:16:54 +0800

    Create 8_4.c

---
+++ 8_4.c
@@ -0,0 +1,12 @@
+/*8_4.位元運算子-奇數與偶數的判斷(10.0)*/
+#include <stdio.h>
+int main() {
+    int num;
+    scanf("%d",&num);
+    if (num%2 ==0){
+        printf("偶數");
+    }else{
+        printf("奇數");
+        }
+   return 0;
+}
