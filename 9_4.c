79c6b81b570c9012b9620c8cdf833a624d611bff
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-03-27 22:49:48 +0800

    Create 9_4.c

---
+++ 9_4.c
@@ -0,0 +1,11 @@
+/*9_4.循序結構隨堂測驗-09:與圓相關的計算(10.0)*/
+#include <stdio.h>
+int main() {
+    float r; // 半徑
+    float pi=3.14159;
+    scanf("%f",&r);
+    
+    printf("%.4f %.4f %.4f",2*r,2*pi*r,pi*r*r);
+
+   return 0;
+}
