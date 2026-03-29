f1e0592fed7a09e2a20639a001fea8b2bd9fbc5f
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-03-29 18:46:20 +0800

    Implement perimeter calculation for a rectangle

---
+++ 10_1.c
@@ -0,0 +1,10 @@
+/*10_1.循序結構競賽挑戰-NOI牧場圍籬(10.0)*/
+#include <stdio.h>
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int perimeter=(2*a)+(2*b);
+    printf("%d",perimeter);
+
+   return 0;
+}
