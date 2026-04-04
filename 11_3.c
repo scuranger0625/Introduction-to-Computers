3b89f2d2bba3ddb76556981de5b929dcdeb1d199
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-04-04 22:40:44 +0800

    Create 11_3.c

---
+++ 11_3.c
@@ -0,0 +1,12 @@
+/*11_3.單一選擇-偶數?(20.0)*/
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if (n%2 == 0){
+        printf("%d是偶數\n",n);
+    }
+    printf("Over!");
+
+   return 0;
+}
