8683e43c5d30d7feddd0400bef1146320ec4ab87
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-04-04 22:43:37 +0800

    Create 11_4.c

---
+++ 11_4.c
@@ -0,0 +1,12 @@
+/*11_4.單一選擇-發現不相等(20.0)*/
+#include <stdio.h>
+int main() {
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if (a != b){
+        printf("%d!=%d\n",a,b);
+    }
+    printf("Over!");
+
+   return 0;
+}
