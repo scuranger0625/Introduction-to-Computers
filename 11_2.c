683a94bb2a01439975b4952bb2e61f28b18fcd9f
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-04-04 22:37:38 +0800

    Create 11_2.c

---
+++ 11_2.c
@@ -0,0 +1,11 @@
+/*11_2.單一選擇-正數?(20.0)*/
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if (n>=1){
+        printf("%d是正數\n",n);
+    }
+    printf("Over!");
+   return 0;
+}
