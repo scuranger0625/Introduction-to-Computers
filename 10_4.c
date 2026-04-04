a23ac03b62f2ede837e77e3d5217d645f4e24221
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-04-04 22:02:06 +0800

    Create 10_4.c

---
+++ 10_4.c
@@ -0,0 +1,9 @@
+/*10_4.循序結構競賽挑戰-UVa10071(10.0)*/
+#include <stdio.h>
+int main() {
+    int v,t;
+    scanf("%d%d",&v,&t);
+    int displacement = 2*v*t;
+    printf("%d",displacement);
+   return 0;
+}
