b9d940dbe86200d76b9d8049b0c0108ed9aae50c
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-04-04 22:33:43 +0800

    Create 11_1.c

---
+++ 11_1.c
@@ -0,0 +1,12 @@
+/*11_1.單一選擇-及格嗎?(20.0)*/
+#include <stdio.h>
+int main() {
+    int score;
+    scanf("%d",&score);
+    if (score>=60){
+        printf("%d分及格\n",score);
+    }
+    printf("Over!\n");
+    
+   return 0;
+}
