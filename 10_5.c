63a7a28e8718fcea32a442fb64f2248158c06010
Leon_LastVerifier <166517099+scuranger0625@users.noreply.github.com>
2026-04-04 22:05:57 +0800

    Create 10_5.c

---
+++ 10_5.c
@@ -0,0 +1,9 @@
+/*10_5.循序結構競賽挑戰-HP CodeWars.Secure the Perimeter(15.0)*/
+#include <stdio.h>
+int main() {
+    int h,w;
+    scanf("%d%d",&h,&w);
+    int p = (2*h)+(2*w);
+    printf("%d",p);
+   return 0;
+}
